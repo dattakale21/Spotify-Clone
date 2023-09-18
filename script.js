4cbfddbb91c6c6439702f089c2e8054a9ec8ba26
dattakale21 <dattakale2008@gmail.com>
2023-09-18 17:51:40 +0530

    some changes

--- script.js
+++ script.js
@@ -123,6 +123,66 @@ document.getElementById('previous').addEventListener('click', () => {    // --> 
 })
 
 
+let menuIcon = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active'); // Toggle the active class on the menu icon
+
+    if (menuIcon.classList.contains('active')) {
+        menuIcon.innerHTML = '<i class="fas fa-times"></i>'; // Replace menu icon with cross icon
+    } else {
+        menuIcon.innerHTML = '<i class="fas fa-bars"></i>'; // Replace cross icon with menu icon
+    }
+
+    navbar.classList.toggle('active'); // Toggle the active class on the navbar
+});
+
 
 // By Datta kale 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu i");
+    const cross = document.querySelector(".cross i");
+    const ho = document.querySelector("#ho");
+
+    menuIcon.addEventListener("click", function () {
+        if (ho.style.display === "block") {
+            ho.style.display = "none";
+            menuIcon.innerHTML = '<i class="fas fa-times"></i>';
+
+        } else {
+            ho.style.display = "block";
+            menuIcon.innerHTML = '<i class="fas fa-bars"></i>'; // Replace cross icon with menu icon
+        }
+    });
+
+
+    cross.addEventListener("click", function () {
+        if (ho.style.display === "block") {
+            ho.style.display = "none";
+            menuIcon.innerHTML = '<i class="fas fa-times"></i>';
+
+        } else {
+            ho.style.display = "none";
+            menuIcon.innerHTML = '<i class="fas fa-bars"></i>'; // Replace cross icon with menu icon
+        }
+    });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const menuIcon = document.querySelector("#m");
+//     const crossIcon = document.querySelector(".cross");
+//     const ho = document.querySelector("#ho");
+
+//     menuIcon.addEventListener("click", function () {
+//         ho.style.display = "block";
+//     });
+
+//     crossIcon.addEventListener("click", function () {
+//         ho.style.display = "none";
+//     });
+// });
+
+
 
